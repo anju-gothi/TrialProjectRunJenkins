10afd69b139a0f3b3b089959ef771249b6b3e2cd
Anju <gothi.anju@gmail.com>
2021-11-29 21:00:44 -0800

    new

--- src/main/java/run/jenkins/trialProject/Trial.java
+++ src/main/java/run/jenkins/trialProject/Trial.java
@@ -1,10 +1,19 @@
 package run.jenkins.trialProject;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Trial {
 	
 	public void getName() {
 		
-		System.out.println("Name of the Employer is Enexus");
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://login.salesforce.com/");
+		
+		//System.out.println("Name of the Employer is Enexus");
 	}
 
 }
